aa6e9494cc924496876dc6359568aef058611302
martinopresnik <martinopresnik@hotmail.com>
2017-05-20 18:18:19 +0200

    feat: Ensamble Added AdaBoost ensamble method

---
+++ Project/src/Ensamble/AdaBoost.py
@@ -0,0 +1,29 @@
+from sklearn.ensemble import AdaBoostClassifier
+
+from src.Shared.DataReader import DataReader as dr
+
+
+class ADAB:
+
+    def __init__(self, features, classificationFeature, classes, trainigSetPath, testSetPath, delimiter):
+
+        reader1 = dr(features, classificationFeature, classes)
+        result1 = reader1.readSet(trainigSetPath, delimiter)
+        self.trainSet = result1["set"]
+        self.trainSetClasses = result1["classes"]
+
+        reader2 = dr(features, classificationFeature, classes)
+        result2 = reader2.readSet(testSetPath, delimiter)
+        self.testSet = result2["set"]
+        self.testSettClasses = result2["classes"]
+        self.clf = None
+
+    def run(self):
+        self.clf = AdaBoostClassifier(n_estimators=100)
+        self.clf.fit(self.trainSet, self.trainSetClasses)
+        score = self.clf.score(self.testSet, self.testSettClasses)
+
+        return {
+            "score": score
+        }
+
