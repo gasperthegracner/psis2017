db33ae48ab627f6d08bb6d6edaca1f2dcc090688
martinopresnik <martinopresnik@hotmail.com>
2017-06-05 21:10:39 +0200

    report: Added ensamble statistics

--- Project/src/Ensamble/AdaBoost.py
+++ Project/src/Ensamble/AdaBoost.py
@@ -1,26 +1,32 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import f1_score
+import random
 
 from Project.src.Shared.DataReader import DataReader as dr
 
 
 class ADAB:
 
-    def __init__(self, features, classificationFeature, classes, trainigSetPath, testSetPath, delimiter):
-
+    def __init__(self, features, classificationFeature, classes, trainigSetPath, testSetPath, delimiter, take_percentage=100, rand_seed=1):
+        self.classificationFeature = classificationFeature
         reader1 = dr(features, classificationFeature, classes)
         result1 = reader1.readSet(trainigSetPath, delimiter)
-        self.trainSet = result1["set"]
-        self.trainSetClasses = result1["classes"]
+        self.trainSet = self.sampling(result1["set"], take_percentage, rand_seed)
+        self.trainSetClasses = self.sampling(result1["classes"], take_percentage, rand_seed)
 
         reader2 = dr(features, classificationFeature, classes)
         result2 = reader2.readSet(testSetPath, delimiter)
-        self.testSet = result2["set"]
-        self.testSettClasses = result2["classes"]
+        self.testSet = self.sampling(result2["set"], take_percentage, rand_seed)
+        self.testSettClasses = self.sampling(result2["classes"], take_percentage, rand_seed)
         self.clf = None
 
-    def run(self):
-        self.clf = AdaBoostClassifier(n_estimators=100)
+    def sampling(self, dset,  percentage, seed):
+        random.seed(seed)
+        k = (len(dset)*percentage)/100
+        return random.sample(list(dset), int(k))
+
+    def run(self, n_estimators = 100):
+        self.clf = AdaBoostClassifier(n_estimators=n_estimators)
         self.clf.fit(self.trainSet, self.trainSetClasses)
         score = self.clf.score(self.testSet, self.testSettClasses)
         predicted = self.clf.predict(self.testSet)
