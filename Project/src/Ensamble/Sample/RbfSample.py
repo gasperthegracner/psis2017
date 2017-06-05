3a889d0d630b998373cd32177068ee24d54d73c2
martinopresnik <martinopresnik@hotmail.com>
2017-06-05 21:10:39 +0200

    report: Added ensamble statistics

--- Project/src/Ensamble/Sample/RbfSample.py
+++ Project/src/Ensamble/Sample/RbfSample.py
@@ -1,4 +1,4 @@
-from Project.src.DecisionTree.DecisionTree import DT
+from Project.src.Ensamble.AdaBoost import ADAB
 import numpy as np
 
 classFeatures = [("mon_intensity", np.float),
@@ -44,49 +44,34 @@ result = [{"score": 0, "f1_score": 0},
 itters = 100
 
 for i in range(0, itters):
-    dt = DT(classFeatures, classificationResult, classes, trainPath, testPath, delimiter=";", take_percentage=70, rand_seed=i)
+    adab = ADAB(classFeatures, classificationResult, classes, trainPath, testPath, delimiter=";", take_percentage=70, rand_seed=i)
 
-    r_tmp = dt.run(
-        criterion='gini',
-        splitter='best',
-        max_depth=None,
-        min_samples_split=2
+    r_tmp = adab.run(
+        n_estimators=10
     )
     result[0]["score"] += r_tmp["score"]
     result[0]["f1_score"] += r_tmp["f1_score"]
 
-    r_tmp = dt.run(
-        criterion='entropy',
-        splitter='best',
-        max_depth=None,
-        min_samples_split=2
+    r_tmp = adab.run(
+        n_estimators=40
     )
     result[1]["score"] += r_tmp["score"]
     result[1]["f1_score"] += r_tmp["f1_score"]
 
-    r_tmp = dt.run(
-        criterion='gini',
-        splitter='random',
-        max_depth=None,
-        min_samples_split=2
+    r_tmp = adab.run(
+        n_estimators=70
     )
     result[2]["score"] += r_tmp["score"]
     result[2]["f1_score"] += r_tmp["f1_score"]
 
-    r_tmp = dt.run(
-        criterion='entropy',
-        splitter='random',
-        max_depth=None,
-        min_samples_split=2
+    r_tmp = adab.run(
+        n_estimators=100
     )
     result[3]["score"] += r_tmp["score"]
     result[3]["f1_score"] += r_tmp["f1_score"]
 
-    r_tmp = dt.run(
-        criterion='gini',
-        splitter='best',
-        max_depth=9,
-        min_samples_split=2
+    r_tmp = adab.run(
+        n_estimators=130
     )
     result[4]["score"] += r_tmp["score"]
     result[4]["f1_score"] += r_tmp["f1_score"]
@@ -96,9 +81,9 @@ for item in result:
     item["f1_score"] = item["f1_score"] / itters
 
 
-print("(gini, best) Mean sample accuracy: " + str(round(result[0]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[0]["f1_score"], 4)))
-print("(entropy, best) Mean sample accuracy: " + str(round(result[1]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[1]["f1_score"], 4)))
-print("(gini, random) Mean sample accuracy: " + str(round(result[2]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[2]["f1_score"], 4)))
-print("(entropy, random) Mean sample accuracy: " + str(round(result[3]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[3]["f1_score"], 4)))
-print("(gini, best, max_depth = 9, min_samples_split = 5) Mean sample accuracy: " + str(round(result[4]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[4]["f1_score"], 4)))
+print("(n_estimators = 10) Mean sample accuracy: " + str(round(result[0]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[0]["f1_score"], 4)))
+print("(n_estimators = 40) Mean sample accuracy: " + str(round(result[1]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[1]["f1_score"], 4)))
+print("(n_estimators = 70) Mean sample accuracy: " + str(round(result[2]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[2]["f1_score"], 4)))
+print("(n_estimators = 100) Mean sample accuracy: " + str(round(result[3]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[3]["f1_score"], 4)))
+print("(n_estimators = 130) Mean sample accuracy: " + str(round(result[4]["score"] * 100, 2)) + "%; F1 score: " + str(round(result[4]["f1_score"], 4)))
 
